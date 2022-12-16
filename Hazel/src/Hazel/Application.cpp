b9c5d0d7d462d08decb219f34e973cc95b3f1e30
Godot <disyourself@qq.com>
2022-12-16 17:51:58 +0800

    Added confusing entry point.( Define in marco in EntryPoint.h, CreateApplicaion function in hazel namespace, implemnet by sand box")

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,21 @@
+#include "Application.h"
+#include <stdio.h>
+
+namespace Hazel {
+
+Application::Application()
+{
+}
+Application::~Application()
+{
+}
+
+void Application::Run()
+{
+	while (true)
+	{
+		printf("Hazel Engine\n");
+	}
+}
+
+}
