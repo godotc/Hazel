c87dfe8f3283718493b6ffa1c8bea5d35bef1333
godotc <disyourself@qq.com>
2023-11-03 23:35:37 +0800

    Mistake 1: freaking random algorithm

--- src/hazel/hazel/renderer/practicle.h
+++ src/hazel/hazel/renderer/practicle.h
@@ -8,21 +8,6 @@
 #include "glm/fwd.hpp"
 
 
-#include <cstdint>
-#include <limits>
-#include <random>
-
-
-
-class Random
-{
-    static std::mt19937                                             s_RandomEngine;
-    static std::uniform_int_distribution<std::mt19937::result_type> s_Distribution;
-
-  public:
-    static void  Init() { s_RandomEngine.seed(std::random_device()()); }
-    static float Float() { return (float)s_Distribution(s_RandomEngine) / (float)std::numeric_limits<uint32_t>::max(); }
-};
 
 struct PraticleProps {
     glm::vec2 Position;
