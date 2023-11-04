3c14391ab16c16844d2df59e4374c7c10cee1264
godot42 <disyourself@qq.com>
2023-11-05 02:21:54 +0800

    just messup the glTextureParateri and glTexParamteri, WTF the old API

--- src/hazel/platform/opengl/opengl_framebuffer.cpp
+++ src/hazel/platform/opengl/opengl_framebuffer.cpp
@@ -19,6 +19,7 @@ OpenGLFrameBuffer::OpenGLFrameBuffer(const FramebufferSpecification &spec)
 
 OpenGLFrameBuffer::~OpenGLFrameBuffer()
 {
+    glDeleteFramebuffers(1, &m_FramebufferID);
 }
 
 void OpenGLFrameBuffer::Bind()
@@ -46,8 +47,8 @@ void OpenGLFrameBuffer::Init()
             glBindTexture(GL_TEXTURE_2D, m_ColorAttachment);
 
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_Specification.Width, m_Specification.Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-            glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-            glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glTextureParameteri(m_ColorAttachment, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTextureParameteri(m_ColorAttachment, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
             glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_ColorAttachment, 0);
         }
@@ -62,7 +63,7 @@ void OpenGLFrameBuffer::Init()
         }
     }
     HZ_CORE_ASSERT(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE, "Framebuffer is incomplete");
-    glBindFramebuffer(GL_TEXTURE_2D, 0);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
 const FramebufferSpecification &OpenGLFrameBuffer::GetSpecification() const
