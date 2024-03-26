f2dfa3ec2680a005980aece67ebfb17d8a7abacd
godot42 <disyourself@qq.com>
2024-03-27 02:44:06 +0800

    2: simple issue  reproduce

--- src/pkgs/vulkan_wrapper/xmake.lua
+++ src/pkgs/vulkan_wrapper/xmake.lua
@@ -12,13 +12,13 @@ add_requires("vulkansdk", {
 
 ---@format disable
 target("vkwrapper")
-    set_kind("static")
+    set_kind("shared")
     add_files("**.cpp")
     add_headerfiles("**.h")
 
     add_includedirs("./", {public = true})
 
     add_packages("vulkansdk", { public = true })
-    set_runtimes("MT")
+    -- set_runtimes("MT")
 
 target_end()
