4167e020335075d96817d7c47701c404b91fb648
godot42 <disyourself@qq.com>
2025-01-11 05:01:57 +0800

    ut update

--- src/pkgs/utility/src/utility/string_utils.cpp
+++ src/pkgs/utility/src/utility/string_utils.cpp
@@ -1,1 +1,65 @@
+//
+/*
+ * @ Author: godot42
+ * @ Modified by: @godot42
+ * @ Modified by: @godot42
+ * @ Modified time: 2025-01-11 03:32:35
+ * @ Description:
+ */
+
 #include "string_utils.h"
+#include <cstddef>
+#include <string_view>
+
+
+std::string string_replace(const std::string &source, const std::string &from, const std::string &to)
+{
+    std::string ret = source;
+    size_t      pos = 0;
+    while ((pos = ret.find(from, pos)) != std::string::npos) {
+        ret.replace(pos, from.size(), to);
+        pos += to.size();
+    }
+    return ret;
+}
+namespace ut {
+namespace str {
+
+
+std::vector<std::string> split(std::string_view source, char delimiter)
+{
+
+    std::vector<std::string> ret;
+    int                      nth = 0;
+    // "abc def"
+    while ((nth = source.find(delimiter)) != std::string::npos) {
+        ret.emplace_back(source.substr(0, nth - 1));
+        source.remove_prefix(nth + 1);
+    }
+    return ret;
+}
+
+bool split(std::string_view source, char sep, std::string_view &left, std::string_view &right)
+{
+    int n = source.find_first_of(sep);
+    if (n == std::string::npos) {
+        return false;
+    }
+    left  = source.substr(0, n);
+    right = source.substr(n + 1, source.size() - n - 1);
+    return true;
+}
+
+bool left(std::string_view source, char delimiter, std::string_view &left)
+{
+    size_t n = source.find_first_of(delimiter);
+    if (n != std::string::npos) {
+        return false;
+    }
+    left = source.substr(0, n);
+    return true;
+}
+
+} // namespace str
+
+} // namespace ut
--- src/pkgs/utility/src/utility/string_utils.h
+++ src/pkgs/utility/src/utility/string_utils.h
@@ -2,54 +2,29 @@
  * @ Author: godot42
  * @ Create Time: 2025-01-06 21:14:30
  * @ Modified by: @godot42
- * @ Modified time: 2025-01-06 22:43:20
- * @ Description:
+ * @ Modified time: 2025-01-11 02:46:12
+ * @ Modified time: 2025-01-11 03:11:16
  */
 
 
 #pragma once
 
 #include <string>
+#include <string_view>
 #include <vector>
 
 
-extern std::string string_replace(const std::string &source, const std::string &from, const std::string &to)
-{
-    std::string ret = source;
-    size_t      pos = 0;
-    while ((pos = ret.find(from, pos)) != std::string::npos) {
-        ret.replace(pos, from.size(), to);
-        pos += to.size();
-    }
-    return ret;
-}
+namespace ut {
+
+std::string string_replace(const std::string &source, const std::string &from, const std::string &to);
 
 
-struct SplitStringResult {
-    std::string left;
-    std::string right;
-};
+namespace str {
+std::vector<std::string> split(std::string_view source, char delimiter = ' ');
+bool                     split(std::string_view source, char sep, std::string_view &left, std::string_view &right);
+std::string_view         left(std::string_view source, std::string_view delimiter);
 
-extern std::vector<std::string> string_split(std::string_view source, char delimiter = ' ')
-{
-    std::vector<std::string> ret;
-    int                      nth = 0;
-    // "abc def"
-    while ((nth = source.find(delimiter)) != std::string::npos) {
-        ret.emplace_back(source.substr(0, nth - 1));
-        source.remove_prefix(nth + 1);
-    }
-    return ret;
-}
+} // namespace str
 
 
-bool string_split(std::string_view source, char sep, std::string_view &left, std::string_view &right)
-{
-    int n = source.find_first_of(sep);
-    if (n == std::string::npos) {
-        return false;
-    }
-    left  = source.substr(0, n);
-    right = source.substr(n + 1, source.size() - n - 1);
-    return true;
-}
+} // namespace ut
